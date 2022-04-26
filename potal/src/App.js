6576fc70c4bc68774648678637334689794a057b
kimsuna <jjona0321@naver.com>
2022-04-26 13:55:42 +0900

    potal practice

---
+++ potal/src/App.js
@@ -0,0 +1,11 @@
+import MyModal from "./MyMordal";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <MyModal />
+    </>
+  );
+}
+
+export default App;
---
+++ potal/src/MyModal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MyModal.css";
+
+const MyModal = () => {
+  return (
+    <div className="MyModal">
+      <div className="content">
+        <h3>이것은 모달</h3>
+        <button>닫기</button>
+      </div>
+    </div>
+  );
+};
